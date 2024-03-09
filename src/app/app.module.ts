63c2c8ec4d16ddcdf2635411ef0d1d55fdec1018
shashankdev21 <shashankchaturvedi68@gmail.com>
2024-03-10 00:37:16 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TopheaderComponent } from './topheader/topheader.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { SearchComponent } from './container/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { ContainerComponent } from './container/container.component';
+import { ProductComponent } from './container/product-list/product/product.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheaderComponent,
+    HeaderComponent,
+    ProductListComponent,
+    MainMenuComponent,
+    TopMenuComponent,
+    SearchComponent,
+    ContainerComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/topheader/topheader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topheader',
+  templateUrl: './topheader.component.html',
+  styleUrl: './topheader.component.css'
+})
+export class TopheaderComponent {
+
+}
